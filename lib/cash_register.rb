637beaddbaed017a966f20c81c8dd718e6b41902
+ <+>
2022-09-01 10:42:23 -0400

    commit tree 1d1836e425777f82aae0e3b3888366aafcdb3aa4

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
+    attr_reader :discount
+    attr_reader :total
 
+    def initialize(discount)
+        @discount = discount
+        @total = 0
+    end
+
+    def total
+        return self.total
+    end
+
+
+end
